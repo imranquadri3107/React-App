d1448b6c63effb4a4a83949079d6817b2da0ba17
imranquadri3107 <imranquadri77@gmail.com>
2019-01-06 20:37:03 -0500

    Update Persons.js

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -3,8 +3,23 @@ import React, {Component} from 'react';
 import Person from './Person/Person';
 
 class Persons extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[Persons.js] Inside Constructor', props);
+
+  }
+
+  componentWillMount() {
+    console.log('[Persons.js] Inside ComponentWillMount')
+  }
+
+  componentDidMount() {
+    console.log('[Persons.js] Inside componenetDidMount')
+  }
+
 
   render(){
+    console.log('[Persons.js] Inside Render()')
     return this.props.persons.map( ( person, index ) => {
       return <Person
         click={() => this.props.clicked( index )}
